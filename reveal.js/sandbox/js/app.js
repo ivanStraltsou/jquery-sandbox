140d6028d40d64ce02043a3d6eb06e09d7b25110
putskoo <putskoo@gmail.com>
2014-04-02 02:02:00 +0300

    sandbox

---
+++ reveal.js/sandbox/js/app.js
@@ -0,0 +1,9 @@
+(function($) {
+
+  $(document).ready(function() {
+
+    $('h1').addClass('strange');
+
+  });
+
+})(jQuery);
---
+++ reveal.js/sandbox/js/traversing.js
@@ -0,0 +1,9 @@
+(function($) {
+
+  $(document).ready(function() {
+
+    console.log( $('.item-list').contents() );
+
+  });
+
+})(jQuery);
